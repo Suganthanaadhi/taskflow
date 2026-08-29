ecdecab1bacf804edeae453646afd336644d5f3b
soorya-2004 <sooryapriya838@gmail.com>
2026-08-29 12:07:07 +0530

    Feat: Add task Function added

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,33 @@ function App() {
   const [tasks, setTasks] = useState([])
   const [input, setInput] = useState('')
 
+  const addTask = () => {
+    if (input.trim() === '') return
+
+    setTasks([...tasks, input])
+    setInput('')
+  }
+
   return (
     <div className="App">
       <h1>TaskFlow App</h1>
+
       <input
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="New task..."
       />
+
+      <button onClick={addTask}>Add Task</button>
+
       <ul id="task-list">
         {tasks.map((task, index) => (
           <li key={index}>{task}</li>
         ))}
-      </ul> <button>Add-Task</button>
+      </ul>
     </div>
   )
 }
 
-export default App// direct push test
+export default App
